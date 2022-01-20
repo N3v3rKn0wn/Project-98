acae3ed9bec84c8205da01ad4b0aafff68ddc2fc
N3v3rKn0wn <74686888+N3v3rKn0wn@users.noreply.github.com>
2022-01-19 18:49:34 -0800

    Update Project98.py

--- Project98.py
+++ Project98.py
@@ -4,10 +4,13 @@ def swapFileData():
     with open('sample1.txt') as f1, open('sample2.txt') as f2:
         data_a = f1.read()
         data_b = f2.read()
+        
+        
+    open('file_1.txt','w').write(data_b)
+    open('file_2.txt','w').write(data_a)
 
-open('file_1.txt','w').write(data_b)
-open('file_2.txt','w').write(data_a)
+    print('Heh you got him')
+    print('data_a: {}, data_b: {}!'.format(file1_contents,file2_contents))
 
-print('Heh you got him')
-print('data_a: {}, data_b: {}!'.format(file1_contents,file2_contents))
+
 swapFileData()
